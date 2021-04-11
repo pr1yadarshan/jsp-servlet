f7a454d0c2b713fbb59b0c700e57e25694abad93
pr1yadarshan <developer.priyadarshan@gmail.com>
2021-04-12 00:03:51 +0530

    explored differences between GET and POST methods

--- src/com/priyadarshan/AddServlet.java
+++ src/com/priyadarshan/AddServlet.java
@@ -7,7 +7,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet{
-	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+	
+	/*
+	 * service allows GET and POST request, however GET request shows the parameters which we are passing in the input fields, won't be secure for passwords
+	 * 
+	 * public void service(HttpServletRequest req, HttpServletResponse res) throws
+	 * IOException { int i = Integer.parseInt(req.getParameter("num1")); int j =
+	 * Integer.parseInt(req.getParameter("num2"));
+	 * 
+	 * res.getWriter().println(i + j); }
+	 */
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		
